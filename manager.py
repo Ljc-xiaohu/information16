1660e871f354ea08919c17e215fe6b7706fedb4b
ljc <763104064@qq.com>
2018-10-18 09:28:52 +0800

    数据库配置

--- manager.py
+++ manager.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+class Config(object):
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:mysql@localhost:3306/information16"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
@@ -8,4 +20,4 @@ def hello_world():
     return "helloworld100"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
