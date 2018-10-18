9df7b743d2433efe44744b06a2e14581e64a4d2a
ljc <763104064@qq.com>
2018-10-18 10:49:42 +0800

    session配置

--- manager.py
+++ manager.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask,session
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 import redis
 from flask_wtf import CSRFProtect
@@ -16,6 +17,11 @@ class Config(object):
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
 
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = 3600*24*2
+
 
 app.config.from_object(Config)
 
@@ -25,11 +31,16 @@ redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT,de
 
 CSRFProtect(app)
 
-@app.route('/',methods=["POST"])
+Session(app)
+
+@app.route('/')
 def hello_world():
 
     redis_store.set("name","laowang")
     print(redis_store.get("name"))
+
+    session["age"] = "13"
+    print(session.get("age"))
 
     return "helloworld100"
 
