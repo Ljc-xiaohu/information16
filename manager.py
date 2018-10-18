c052b0be7cffc01cf3de0f3f55e0584163c19ef2
ljc <763104064@qq.com>
2018-10-18 10:34:33 +0800

    csrf保护配置

--- manager.py
+++ manager.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import redis
+from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
 
 class Config(object):
     DEBUG = True
+
+    SECRET_KEY = "hsdfgfgafv"
 
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:mysql@localhost:3306/information16"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -20,7 +23,9 @@ db = SQLAlchemy(app)
 
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT,decode_responses=True)
 
-@app.route('/')
+CSRFProtect(app)
+
+@app.route('/',methods=["POST"])
 def hello_world():
 
     redis_store.set("name","laowang")
